53041459974abf76d048e04adf9889a47f226680
ericardomuten <eraraya.ricardo19@gmail.com>
2024-10-23 17:31:04 +0000

    Setup the setup.py and requirements.py.

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setuptools.setup(
      name="masters_thesis_tools",     
      version="0.0.1",
      python_requires=">=3.10",   
-     packages=["qaoa_tools, qrr_tools"],
+     packages=["qaoa_tools", "qrr_tools"],
 )
