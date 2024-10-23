45e8f85ab87af9bc812ee4ef0814ede286bc8b06
ericardomuten <eraraya.ricardo19@gmail.com>
2024-10-23 17:51:06 +0000

    Update setup.py.

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 setuptools.setup(     
      name="masters_thesis_tools",     
      version="0.0.1",
-     python_requires=">=3.10",   
+     python_requires=">=3.10",
+     install_requires=[
+          'gurobipy==11.0.3',
+      ],
      packages=["qaoa_tools", "qrr_tools"],
 )
